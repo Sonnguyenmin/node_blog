7e8fee741e76fb8448a35ea24db76b59aa2c120c
Sonnguyenmin <nguyensoncl184@gmail.com>
2023-03-21 00:37:49 +0700

    bài 2 express-handlerbars

--- src/Style.js
+++ src/Style.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const app = express();
-const path = require("path"); 
+const path = require('path');
 const morgan = require('morgan');
-const exphbs = require("express-handlebars"); 
-const { engine } = require("express-handlebars"); 
+const exphbs = require("express-handlebars");
+const {engine} = require('express-handlebars');
 
-//Http logger morgan
+//http logger
 app.use(morgan('combined'));
 
-//template engine
+//template enginer
 app.engine('hbs', engine({
-  extname: '.hbs',
-}));
+    extname: '.hbs'
+  }));
 app.set('view engine', 'hbs');
 
 app.use(express.json());
-app.set("views", path.join(__dirname, "resource/views"));
+app.set("views", path.join(__dirname,"resource/views"));
 
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.get('/news', (req, res) => {
+  res.render('news');
 });
 
 app.listen(3000);
